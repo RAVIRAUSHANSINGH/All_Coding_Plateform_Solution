3bfa03731ee5159dde6c4677be5a5f96845a929d
MangloVedic <raviraushanlyf@gmail.com>
2022-01-12 23:34:49 +0530

    CPP

---
+++ GFG/Array/Missing number in array.cpp
@@ -0,0 +1,7 @@
+class Solution{
+  public:
+    int MissingNumber(vector<int>& array, int n) {
+        // Your code goes here
+        return (n*(n+1))/2 - accumulate(array.begin(),array.end(),0);
+    }
+};
