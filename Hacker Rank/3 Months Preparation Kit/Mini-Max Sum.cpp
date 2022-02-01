fd0b957631b0e01cb636de914002d4a99c142057
MangloVedic <54463145+RAVIRAUSHANSINGH@users.noreply.github.com>
2022-02-02 03:08:56 +0530

    Delete Mini-Max Sum.cpp

--- Hacker Rank/3 Months Preparation Kit/Mini-Max Sum.cpp
+++
@@ -1,9 +0,0 @@
-void miniMaxSum(vector<int> arr) {
-    sort(arr.begin(),arr.end());
-    long long sum=0;
-    for(int i = 0; i < 5; i++){
-        sum += arr[i];
-    }
-    cout << sum-arr[4] << " " << sum-arr[0] << endl;
-    
-}
