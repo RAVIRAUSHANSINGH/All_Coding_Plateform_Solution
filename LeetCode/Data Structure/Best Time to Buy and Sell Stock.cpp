878d50a414098dc86f263fb2e889bfaaf17e8766
MangloVedic <RAVIRAUSHANLYF@GMAIL.COM>
2022-02-02 22:07:17 +0530

    2 CPP Solution

--- LeetCode/Data Structure/Best Time to Buy and Sell Stock.cpp
+++ LeetCode/Data Structure/Best Time to Buy and Sell Stock.cpp
@@ -23,3 +23,23 @@ public:
         
     }
 };
+
+// WITHOUT EXTRA SPACE
+
+// SOLUTION 2
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min=prices[0];
+        int profit=0;
+        for(int i=1;i<prices.size();i++){
+            if(prices[i]<min){
+                min=prices[i];
+            }
+            int diff=prices[i]-min;
+            profit=max(profit,diff);
+        }
+        return profit;
+    }
+};
