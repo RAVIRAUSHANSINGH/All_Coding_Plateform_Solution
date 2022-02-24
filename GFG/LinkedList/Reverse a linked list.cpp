9bbef7943fc622ee16b8014a86e29356a8db6533
MangloVedic <RAVIRAUSHANLYF@GMAIL.COM>
2022-02-24 17:20:46 +0530

    CPP

--- GFG/LinkedList/Reverse a linked list.cpp
+++ GFG/LinkedList/Reverse a linked list.cpp
@@ -5,23 +5,22 @@ class Solution
     struct Node* reverseList(struct Node *head)
     {
         if(head==NULL || head->next==NULL){
-           return head;
+           return head;  //for one sized LL
        }
-       Node* curr = head;
-       Node* prev = NULL;
-       Node* forward = NULL;
-       
-       while(curr!=NULL){
-       forward = curr->next;
-       curr->next = prev;
-       prev = curr;
-       curr = forward;
+       Node *prevptr=NULL; //1stptr for prev
+       Node *currptr=head; //2nd ptr for curr
+       Node*nextptr;       //3rdptr for next so that we dont loose coonection with next
+       while(currptr!=NULL){//if we stop just one step before then last node will not be reverse
+           nextptr=currptr->next; //storing current ka next in nextpointer
+           currptr->next=prevptr; //pointing current pointe/head to previous ptr which is null first step for reversing
+
+          //Now we will be move each pointer one step ahead
+           prevptr=currptr; //moving previous ptr one step ahead
+           currptr=nextptr;//moving current ptr on step ahead
+           //current ptr will move one step at every run of while loop
        }
-       return prev;
-   
+       return prevptr;
     }
     
+    
 };
-
-
-//Problem:= https://practice.geeksforgeeks.org/problems/reverse-a-linked-list/1
