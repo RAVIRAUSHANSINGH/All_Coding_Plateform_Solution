425276cccfe56d61e52f7f6bd3ec10bb73ddc856
MangloVedic <RAVIRAUSHANLYF@GMAIL.COM>
2022-02-25 21:23:45 +0530

    CPP

---
+++ GFG/LinkedList/Intersection Point in Y Shapped Linked Lists.cpp
@@ -0,0 +1,45 @@
+int length(Node* head){
+    int l=0;
+    Node *temp=head;
+    while(temp!=NULL){
+        temp=temp->next;
+        l++;
+    }
+    return l;
+}
+
+//Function to find intersection point in Y shaped Linked Lists.
+int intersectPoint(Node* head1, Node* head2)
+{
+    // Your Code Here
+    int l1=length(head1);
+    int l2=length(head2);
+    int d=0;
+    Node *ptr1;
+    Node *ptr2;
+    if(l1>l2){
+        d=l1-l2;
+        ptr1=head1;
+        ptr2=head2;
+        
+    }else{
+        d=l2-l1;
+        ptr2=head1;
+        ptr1=head2;
+    }
+    while(d){
+        ptr1=ptr1->next;
+        if(ptr1==NULL){
+            return -1;
+        }
+        d--;
+    }while(ptr1!=NULL and ptr2!=NULL){
+        if(ptr1==ptr2){
+            return ptr1->data;
+        }
+        ptr1=ptr1->next;
+         ptr2=ptr2->next;
+    }
+    return -1;
+}
+
