22a630b1d141667594be320ceea96305f310e68c
MangloVedic <54463145+RAVIRAUSHANSINGH@users.noreply.github.com>
2022-04-16 11:20:07 +0530

    CPP

---
+++ LeetCode/Data Structure/Daily Challenge/538. Convert BST to Greater Tree.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int sum=0;
+     void dfs(TreeNode* root){
+        if(root==NULL) return;
+        dfs(root->right);
+        root->val+=sum;
+        sum=root->val;
+        dfs(root->left);
+        return;
+        }
+      
+    TreeNode* convertBST(TreeNode* root) {
+        dfs(root);
+            return root;
+    }
+};
